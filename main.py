12397b2692e7d27fd2471a5f1348dc0682a4b1a0
Raviteja <ravitejamekala02@gmail.com>
2024-03-13 23:29:13 +0530

    update

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import requests
 import json
 if __name__ == '__main__':
 
+    api = "ae605cae1d3e4b2eba9171522241303"
     city = input("Enter your city name:  ")
-    weather_info = requests.get(f"http://api.weatherapi.com/v1/current.json?key=ae605cae1d3e4b2eba9171522241303&q={city}")
+    weather_info = requests.get(f"http://api.weatherapi.com/v1/current.json?key={api}&q={city}")
     json_data = json.loads(weather_info.text)
     print(json_data['current']['temp_c'])
 
