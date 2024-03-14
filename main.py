8ff357d83486fb878a79d30b782256a1addcae66
Raviteja <ravitejamekala02@gmail.com>
2024-03-14 21:11:59 +0530

    update

--- main.py
+++ main.py
@@ -6,5 +6,7 @@ if __name__ == '__main__':
     city = input("Enter your city name:  ")
     weather_info = requests.get(f"http://api.weatherapi.com/v1/current.json?key={api}&q={city}")
     json_data = json.loads(weather_info.text)
-    print(json_data['current']['temp_c'])
+    print("Temperature", json_data['current']['temp_c'])
+    print("Wind (km/hr)", json_data['current']['wind_kph'])
+    print("Humidity", json_data['current']['humidity'])
 
